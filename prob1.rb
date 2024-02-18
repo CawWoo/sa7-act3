0281d05d16d7fe91e112db8778d5a4c102f522ab
CawWoo <melodicraven@gmail.com>
2024-02-17 23:48:27 -0600

    Imported CSV library, parsed data, and made hash

--- prob1.rb
+++ prob1.rb
@@ -1,1 +1,12 @@
 #CSV parser
+
+#Implement a simple CSV (Comma-Separated Values) parser that reads data from a CSV file and transforms it into an array of hashes,
+#with each hash representing a row of the CSV.
+
+require 'csv'
+
+CSV.foreach(("data.csv"), headers: true, col_sep: ",") do |line|
+  person = {}
+  line.each{|i, v| person[i] = v}
+  puts person
+end
